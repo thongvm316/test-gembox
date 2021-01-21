62df8b2bac6c93aa5553ebaea9f9b3773735b92a
thongvm316 <56250318+thongvm316@users.noreply.github.com>
2021-01-21 22:45:04 +0700

    20200121

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import * as serviceWorker from './serviceWorker';
-import Root from './root/Root';
-import 'antd/dist/antd.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.scss'
+import * as serviceWorker from './serviceWorker'
+import Root from './root/Root'
+import 'antd/dist/antd.css'
 import setAuthToken from './utils/setAuthToken'
 
 if (localStorage.getItem('token')) {
-    setAuthToken(localStorage.getItem('token'))
+  setAuthToken(localStorage.getItem('token'))
 } else if (localStorage.getItem('token-user')) {
-    setAuthToken(localStorage.getItem('token-user'))
+  setAuthToken(localStorage.getItem('token-user'))
 }
 
-ReactDOM.render(<Root />, document.getElementById('root'));
-serviceWorker.unregister();
+ReactDOM.render(<Root />, document.getElementById('root'))
+serviceWorker.unregister()
--- todo.md
+++ todo.md
@@ -24,14 +24,13 @@ At evening
 
 2. Hover icon in sider - Problem with Sider when click
 3. Style DatePicker Home Page - Chang month to Kr language in P2, P3
-4. Map fake data to chart: page 2 - page 3
-5. Lazy load and add lastindex - Redux
+4. Lazy load and add lastindex - Redux
 
 Fix Method Call API like Easy FrontEnd -- https://www.youtube.com/watch?v=LXz_7WQpob4
 Research - Lodash // Code with less err in solution of EasyFrontEnd
 
 ---
 
-1. PrivateRouter ---- Done
-2. Show name in header
-3. Can't perform a React state update on an unmounted component. This is a no-op, but it indicates a memory leak in your application. To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
+1. Map fake data to chart: page 2 - page 3
+2. UI when render data
+3. Solution for redux
