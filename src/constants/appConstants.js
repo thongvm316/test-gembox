fbd14d2adfb68c315875e61fc13d924e5a96375e
thongvm316 <vominhthong1603@gmail.com>
2021-01-22 17:08:59 +0700

    ABN

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,4 +1,4 @@
 // export const API_URL = 'https://gemfactory.brickmate.kr';
-// export const API_URL =
-//   'http://ec2-54-180-132-160.ap-northeast-2.compute.amazonaws.com'
-export const API_URL = 'http://192.168.1.76'
+export const API_URL =
+  'http://ec2-54-180-132-160.ap-northeast-2.compute.amazonaws.com'
+// export const API_URL = 'http://192.168.1.76'
