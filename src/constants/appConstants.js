866bf7ab85c9f39782ae006d2ffe5998ef6d27c1
ThongVM <vominhthong1603@gmail.com>
2021-02-08 10:04:27 +0700

    fix

--- src/constants/appConstants.js
+++ src/constants/appConstants.js
@@ -1,5 +1,5 @@
-export const API_URL =
-  'http://ec2-54-180-132-160.ap-northeast-2.compute.amazonaws.com'
+// export const API_URL =
+'http://ec2-54-180-132-160.ap-northeast-2.compute.amazonaws.com'
 // export const API_URL = 'http://192.168.1.76'
 
 export const market_list = [
